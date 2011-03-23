46e4ab97e43eaf95b55154ec420e3e0965f0524a
Ubuntu <ubuntu@ip-10-243-25-187.ec2.internal>
2011-03-23 17:25:23 +0000

    Use real time search

--- news/search/search_indexes.py
+++ news/search/search_indexes.py
@@ -2,12 +2,15 @@ from haystack.indexes import *
 from haystack import site
 from search.models import *
 
-class PageIndex(SearchIndex):
+class PageIndex(RealTimeSearchIndex):
   text = CharField(document=True, use_template=True)
   first_analysed = DateTimeField(model_attr='first_analysed')
   
   def get_queryset(self):
     return Page.objects.filter(first_analysed__isnull=False)
+
+  def get_updated_field(self):
+    return 'last_analysed'
 
   def should_update(self, instance, **kwargs):
     return instance.first_analysed != None
